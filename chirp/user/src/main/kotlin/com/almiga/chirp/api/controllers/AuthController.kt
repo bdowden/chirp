d8ceb27c0a4da3713abe149500210c474f034f46
Ben Dowden <bdowden@gmail.com>
2025-09-19 22:40:50 -0400

    add controllers and mappers

---
+++ chirp/user/src/main/kotlin/com/almiga/chirp/api/controllers/AuthController.kt
@@ -0,0 +1,27 @@
+package com.almiga.chirp.api.controllers
+
+import com.almiga.chirp.api.dto.RegisterRequest
+import com.almiga.chirp.api.dto.UserDto
+import com.almiga.chirp.api.mappers.toUserDto
+import com.almiga.chirp.service.auth.AuthService
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/auth")
+class AuthController(private val authService: AuthService) {
+
+    @PostMapping("/register")
+    fun register(
+        @Valid @RequestBody body: RegisterRequest
+    ): UserDto {
+        return authService.register(
+            email = body.email,
+            username = body.username,
+            password = body.password
+        ).toUserDto()
+    }
+}
