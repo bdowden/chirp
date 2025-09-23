48f958d031189bee3d9a32148e85c66bcfca04fc
Ben Dowden <bdowden@gmail.com>
2025-09-23 17:54:47 -0400

    add actually logging in

--- chirp/user/src/main/kotlin/com/almiga/chirp/api/controllers/AuthController.kt
+++ chirp/user/src/main/kotlin/com/almiga/chirp/api/controllers/AuthController.kt
@@ -1,7 +1,10 @@
 package com.almiga.chirp.api.controllers
 
+import com.almiga.chirp.api.dto.AuthenticatedUserDto
+import com.almiga.chirp.api.dto.LoginRequest
 import com.almiga.chirp.api.dto.RegisterRequest
 import com.almiga.chirp.api.dto.UserDto
+import com.almiga.chirp.api.mappers.toAuthenticatedUserDto
 import com.almiga.chirp.api.mappers.toUserDto
 import com.almiga.chirp.service.auth.AuthService
 import jakarta.validation.Valid
@@ -23,5 +26,15 @@ class AuthController(private val authService: AuthService) {
             username = body.username,
             password = body.password
         ).toUserDto()
+    }
+
+    @PostMapping("/login")
+    fun login(
+        @RequestBody body: LoginRequest
+    ): AuthenticatedUserDto {
+        return authService.login(
+            email = body.email,
+            password = body.password
+        ).toAuthenticatedUserDto()
     }
 }
