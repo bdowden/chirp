d6266b5e4c65f40288dd6293a1aab181c4e10f77
bdowden <bdowden@gmail.com>
2025-12-18 17:45:40 -0500

    Add Design components (#5)
    * Add buttons
    
    * Add icon button
    
    * FAB
    
    * standard text field
    
    * Add password text field
    
    * Add basic surface layout
    
    * Add adaptive form layout
    
    * Add adaptive result layout
    
    * Add success layout and success icon
    
    * Add snackbar scaffold

--- mobile/kmp/core/designsystem/build.gradle.kts
+++ mobile/kmp/core/designsystem/build.gradle.kts
@@ -8,7 +8,9 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
+                implementation(projects.core.presentation)
                 implementation(compose.components.resources)
+                implementation(compose.components.uiToolingPreview)
             }
         }
 
