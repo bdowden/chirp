fa242ff1b35b14dd29c7ea395fc774c6dd2c11b6
Ben Dowden <bdowden@gmail.com>
2025-12-20 15:39:57 -0500

    Add register api

---
+++ mobile/kmp/core/data/src/commonMain/kotlin/com/almiga/core/data/auth/KtorAuthService.kt
@@ -0,0 +1,28 @@
+package com.almiga.core.data.auth
+
+import com.almiga.core.data.dto.request.RegisterRequest
+import com.almiga.core.data.networking.post
+import com.almiga.core.domain.auth.AuthService
+import com.almiga.core.domain.util.DataError
+import com.almiga.core.domain.util.EmptyResult
+import io.ktor.client.HttpClient
+
+class KtorAuthService(
+    private val httpClient: HttpClient
+): AuthService {
+
+    override suspend fun register(
+        email: String,
+        username: String,
+        password: String
+    ): EmptyResult<DataError.Remote> {
+        return httpClient.post(
+            route = "/auth/register",
+            body = RegisterRequest(
+                email = email,
+                username = username,
+                password = password
+            )
+        )
+    }
+}
---
+++ mobile/kmp/core/domain/src/commonMain/kotlin/com/almiga/core/domain/auth/AuthService.kt
@@ -0,0 +1,12 @@
+package com.almiga.core.domain.auth
+
+import com.almiga.core.domain.util.DataError
+import com.almiga.core.domain.util.EmptyResult
+
+interface AuthService {
+    suspend fun register(
+        email: String,
+        username: String,
+        password: String
+    ): EmptyResult<DataError.Remote>
+}
