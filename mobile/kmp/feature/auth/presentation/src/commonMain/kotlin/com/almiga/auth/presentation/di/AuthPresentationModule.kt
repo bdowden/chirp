25980b6c72f642d36d14ec5bd8240fcd4f5e1bc8
Ben Dowden <bdowden@gmail.com>
2025-12-23 12:04:18 -0500

    Login screen

--- mobile/kmp/feature/auth/presentation/src/commonMain/kotlin/com/almiga/auth/presentation/di/AuthPresentationModule.kt
+++ mobile/kmp/feature/auth/presentation/src/commonMain/kotlin/com/almiga/auth/presentation/di/AuthPresentationModule.kt
@@ -1,6 +1,7 @@
 package com.almiga.auth.presentation.di
 
 import com.almiga.auth.presentation.emailVerification.EmailVerificationViewModel
+import com.almiga.auth.presentation.login.LoginViewModel
 import com.almiga.auth.presentation.register.RegisterViewModel
 import com.almiga.auth.presentation.success.RegisterSuccessViewModel
 import org.koin.core.module.dsl.viewModelOf
@@ -10,4 +11,5 @@ val authPresentationModule = module {
     viewModelOf(::RegisterViewModel)
     viewModelOf(::RegisterSuccessViewModel)
     viewModelOf(::EmailVerificationViewModel)
+    viewModelOf(::LoginViewModel)
 }
---
+++ mobile/kmp/feature/auth/presentation/src/commonMain/kotlin/com/almiga/auth/presentation/login/LoginAction.kt
@@ -0,0 +1,10 @@
+package com.almiga.auth.presentation.login
+
+sealed interface LoginAction {
+    data object OnTogglePasswordVisibility: LoginAction
+    data object OnForgotPasswordClick: LoginAction
+    data object OnLoginClick: LoginAction
+    data object OnSignUpClick: LoginAction
+}
+
+
