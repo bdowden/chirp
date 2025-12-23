5aa82965e9d6288199840a886d2643c995e497c6
Ben Dowden <bdowden@gmail.com>
2025-12-23 17:50:04 -0500

    Scoped view models

--- mobile/kmp/composeApp/src/commonMain/kotlin/com/almiga/chirp/di/initKoin.kt
+++ mobile/kmp/composeApp/src/commonMain/kotlin/com/almiga/chirp/di/initKoin.kt
@@ -3,6 +3,7 @@ package com.almiga.chirp.di
 import com.almiga.auth.presentation.di.authPresentationModule
 import com.almiga.chat.presentation.di.chatPresentationModule
 import com.almiga.core.data.di.coreDataModule
+import com.almiga.core.presentation.di.corePresentationModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
@@ -14,6 +15,7 @@ fun initKoin(config: KoinAppDeclaration? = null) {
             authPresentationModule,
             appModule,
             chatPresentationModule,
+            corePresentationModule,
         )
     }
 }
