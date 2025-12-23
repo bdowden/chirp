5af08fc833c6c15cb150c4d28f12dc32b9f2ee91
Ben Dowden <bdowden@gmail.com>
2025-12-23 12:55:22 -0500

    token refresh implementation

--- mobile/kmp/core/data/src/commonMain/kotlin/com/almiga/core/data/networking/HttpClientFactory.kt
+++ mobile/kmp/core/data/src/commonMain/kotlin/com/almiga/core/data/networking/HttpClientFactory.kt
@@ -1,23 +1,35 @@
 package com.almiga.core.data.networking
 
 import com.almiga.core.data.BuildKonfig
+import com.almiga.core.data.dto.AuthInfoSerializable
+import com.almiga.core.data.dto.request.RefreshRequest
+import com.almiga.core.data.mappers.toDomain
+import com.almiga.core.domain.auth.SessionStorage
 import com.almiga.core.domain.loggin.ChirpLogger
+import com.almiga.core.domain.util.onFailure
+import com.almiga.core.domain.util.onSuccess
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
+import io.ktor.client.statement.request
 import io.ktor.http.ContentType
 import io.ktor.http.ContentType.Application.Json
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.serialization.json.Json
 
 class HttpClientFactory(
     private val chirpLogger: ChirpLogger,
+    private val sessionStorage: SessionStorage,
 ) {
     fun create(engine: HttpClientEngine) : HttpClient{
         return HttpClient(engine) {
@@ -47,6 +59,54 @@ class HttpClientFactory(
                 header("x-api-key", BuildKonfig.API_KEY)
                 contentType(Json)
 
+            }
+
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        sessionStorage
+                            .observeAuthInfo()
+                            .firstOrNull()
+                            ?.let {
+                                BearerTokens(
+                                    accessToken = it.accessToken,
+                                    refreshToken = it.refreshToken
+                                )
+                            }
+                    }
+                    refreshTokens {
+                        if(response.request.url.encodedPath.contains("auth/")) {
+                            return@refreshTokens null
+                        }
+
+                        val authInfo = sessionStorage.observeAuthInfo().firstOrNull()
+                        if(authInfo?.refreshToken.isNullOrBlank()) {
+                            sessionStorage.set(null)
+                            return@refreshTokens null
+                        }
+
+                        var bearerTokens: BearerTokens? = null
+                        client.post<RefreshRequest, AuthInfoSerializable>(
+                            route = "/auth/refresh",
+                            body = RefreshRequest(
+                                refreshToken = authInfo.refreshToken
+                            ),
+                            builder = {
+                                markAsRefreshTokenRequest()
+                            }
+                        ).onSuccess { newAuthInfo ->
+                            sessionStorage.set(newAuthInfo.toDomain())
+                            bearerTokens = BearerTokens(
+                                accessToken = newAuthInfo.accessToken,
+                                refreshToken = newAuthInfo.refreshToken
+                            )
+                        }.onFailure { error ->
+                            sessionStorage.set(null)
+                        }
+
+                        bearerTokens
+                    }
+                }
             }
         }
     }
