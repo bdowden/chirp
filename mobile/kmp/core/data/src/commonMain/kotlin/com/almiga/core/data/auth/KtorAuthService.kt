f096f63cb9fdec89f9954d13a7e1a0f2ae8d9aff
Ben Dowden <bdowden@gmail.com>
2025-12-23 11:46:04 -0500

    Email verification

--- mobile/kmp/core/data/src/commonMain/kotlin/com/almiga/core/data/auth/KtorAuthService.kt
+++ mobile/kmp/core/data/src/commonMain/kotlin/com/almiga/core/data/auth/KtorAuthService.kt
@@ -2,6 +2,7 @@ package com.almiga.core.data.auth
 
 import com.almiga.core.data.dto.request.EmailRequest
 import com.almiga.core.data.dto.request.RegisterRequest
+import com.almiga.core.data.networking.get
 import com.almiga.core.data.networking.post
 import com.almiga.core.domain.auth.AuthService
 import com.almiga.core.domain.util.DataError
@@ -31,6 +32,13 @@ class KtorAuthService(
         return httpClient.post(
             route = "/auth/resend-verification",
             body = EmailRequest(email),
+        )
+    }
+
+    override suspend fun verifyEmail(token: String): EmptyResult<DataError.Remote> {
+        return httpClient.get(
+            route = "/auth/verify",
+            queryParams = mapOf("token" to token)
         )
     }
 }
--- mobile/kmp/feature/auth/presentation/src/commonMain/kotlin/com/almiga/auth/presentation/di/AuthPresentationModule.kt
+++ mobile/kmp/feature/auth/presentation/src/commonMain/kotlin/com/almiga/auth/presentation/di/AuthPresentationModule.kt
@@ -1,5 +1,6 @@
 package com.almiga.auth.presentation.di
 
+import com.almiga.auth.presentation.emailVerification.EmailVerificationViewModel
 import com.almiga.auth.presentation.register.RegisterViewModel
 import com.almiga.auth.presentation.success.RegisterSuccessViewModel
 import org.koin.core.module.dsl.viewModelOf
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val authPresentationModule = module {
     viewModelOf(::RegisterViewModel)
     viewModelOf(::RegisterSuccessViewModel)
+    viewModelOf(::EmailVerificationViewModel)
 }
